e345c580ee19197c5662202d992c664515e7e92d
ArthurPeyre <peyre.arthur@outlook.fr>
2026-07-29 22:52:45 +0200

    Mise en place du Hashage des mots de passe User

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -1,18 +1,44 @@
-type GeneratedFields = "id" | "createdAt";
-type UserInput = Omit<User, GeneratedFields> & Partial<Pick<User, GeneratedFields>>;
+import bcrypt from "bcrypt";
 
-export class User {
+const SALT_ROUNDS = 10;
+
+interface UserData {
     id: number | null;
     email: string;
     name: string;
-    password: string;
+    passwordHash: string;
+    createdAt: Date | null;
+}
+
+type GeneratedFields = "id" | "createdAt";
+type UserInput = Omit<UserData, GeneratedFields> &
+    Partial<Pick<UserData, GeneratedFields>>;
+
+export class User implements UserData {
+    id: number | null;
+    email: string;
+    name: string;
+    passwordHash: string;
     createdAt: Date | null;
 
     constructor(data: UserInput) {
         this.id = data.id ?? null;
         this.email = data.email;
         this.name = data.name;
-        this.password = data.password
+        this.passwordHash = data.passwordHash;
         this.createdAt = data.createdAt ?? null;
+    }
+
+    static async create(data: {
+        email: string;
+        name: string;
+        password: string;
+    }): Promise<User> {
+        const passwordHash = await bcrypt.hash(data.password, SALT_ROUNDS);
+        return new User({ email: data.email, name: data.name, passwordHash });
+    }
+
+    verifyPassword(password: string): Promise<boolean> {
+        return bcrypt.compare(password, this.passwordHash);
     }
 }
