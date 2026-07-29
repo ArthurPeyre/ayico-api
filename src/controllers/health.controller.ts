f7dc20db9444126e304cbfc37c74a3a703fbec76
ArthurPeyre <peyre.arthur@outlook.fr>
2026-07-29 20:34:34 +0200

    Ajout de l'enum de HTTP Status Code

--- src/controllers/health.controller.ts
+++ src/controllers/health.controller.ts
@@ -1,5 +1,6 @@
 import type { FastifyReply, FastifyRequest } from "fastify";
 import { getApiHealth, getDatabaseHealth } from "../services/health.service";
+import { HttpStatusCode as HSC } from "../utils/HttpStatusCode";
 
 export async function getHealth(_req: FastifyRequest, _reply: FastifyReply) {
     return getApiHealth();
@@ -7,6 +8,6 @@ export async function getHealth(_req: FastifyRequest, _reply: FastifyReply) {
 
 export async function getDbHealth(_req: FastifyRequest, reply: FastifyReply) {
     const result = await getDatabaseHealth();
-    if (result.status !== "ok") reply.code(503);
+    if (result.status !== "ok") reply.code(HSC.SERVICE_UNAVAILABLE);
     return result;
 }
