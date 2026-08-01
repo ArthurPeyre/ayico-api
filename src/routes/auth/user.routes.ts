bfa50892ef3abd3e6f97f9093d8394e73d1fb521
ArthurPeyre <peyre.arthur@outlook.fr>
2026-08-01 15:34:49 +0200

    Init branch 'develop' + Prettier

--- src/routes/auth/user.routes.ts
+++ src/routes/auth/user.routes.ts
@@ -3,5 +3,9 @@ import { UserController } from "../../controllers/auth/user.controller";
 
 export async function userRoutes(app: FastifyInstance) {
     app.post("/user", UserController.createUser);
-    app.get("/users", { preHandler: [app.authenticate] }, UserController.getAllUsers);
+    app.get(
+        "/users",
+        { preHandler: [app.authenticate] },
+        UserController.getAllUsers,
+    );
 }
